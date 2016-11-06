7469039ae1d5a3a90adc515aa396b1f47ec5796a
anybalrocha <anybalrocha@hotmail.com>
2016-11-06 17:20:08 -0200

    Inclusão de projeto.

---
+++ MVC/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        public ActionResult Create()
+        {
+            var role = new IdentityRole();
+            return View(role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole model)
+        {
+            context.Roles.Add(model);
+            context.SaveChanges();
+            return View("Index", context.Roles.ToList());
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ASP.NET MVC - Segurança
+
+Projeto destinado aos desenvolvedores ASP.NET MVC, com o objetivo de apresentar os recursos do ASP.NET Identity, com destaques nos pontos onde se deve ter atenção e aplicar a proteção correta para que usuários mal intencionados não executem comandos e ações que prejudiquem seu sistema.
